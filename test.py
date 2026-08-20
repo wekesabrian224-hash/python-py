2fb29c88f538fe87b5ff2a882baa6a1b0a7b63ce
wekesabrian224-hash <wekesabrian224@gmail.com>
2026-08-20 10:12:54 +0300

    my python notes

---
+++ test.py
@@ -0,0 +1,56 @@
+# print("Hello Lucky!")
+# The print() function is used to display text or values.
+# Texts in Python use either double quotes (" ") or single quotes (' ').
+
+
+# VARIABLES
+# Python has no command for declaring variables.
+x = 5
+y = "Sally"
+
+
+# TYPE FUNCTION
+# To get the type of a variable, we use the type() function.
+# Example:
+# print(type(x))
+# print(type(y))
+
+
+# CASE SENSITIVE
+# Python is case-sensitive.
+# This means x and X are different variables.
+
+
+# RULES FOR NAMING VARIABLES
+#
+# 1. The variable must start with a letter or an underscore (_).
+#
+# 2. The variable name cannot start with a number.
+#
+# 3. The variable name can only contain alphanumeric characters
+#    (A-Z, a-z, 0-9) and underscores (_).
+#
+# 4. Variable names are case-sensitive.
+#
+# 5. Variable names cannot be Python keywords.
+
+
+# MULTI-WORD VARIABLE NAMES
+# For names with multiple words, we can use:
+#
+# camelCase
+# PascalCase
+# snake_case
+#
+# Examples:
+# firstName = "Lucky"
+# FirstName = "Lucky"
+# first_name = "Lucky"
+
+
+# PRINT FUNCTION
+# print() is used to output variables or values.
+#
+# Example:
+# print(x)
+# print(y)
